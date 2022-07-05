20fce1d166c52461b632a643e22176d1e763a2f3
20025732 <20025732@rp.edu.sg>
2022-07-05 12:57:56 +0800

    commit message goes here

--- web/test.php
+++ web/test.php
@@ -1,7 +1,10 @@
 <!-- fix : <html> element does not have a [lang] attribute -->
     <!--: <html> element does not have a [title] attribute-->
+<!DOCTYPE html>
 <html lang="en">
     <title>Hello World!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Put your description here.">
     <head>
     </head>
     <body>
